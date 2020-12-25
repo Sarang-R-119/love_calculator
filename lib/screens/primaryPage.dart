a175a3a5707ea424a2613d21604b4dc2ab3dc8e7
Sarang-R-119 <sarangvr@yahoo.com>
2020-12-25 13:05:28 +0400

    Added controllers to the text fields and can request from API via http. Learned (http requests, controllers)

--- lib/screens/primaryPage.dart
+++ lib/screens/primaryPage.dart
@@ -1,17 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:love_calculator/widgets/name_container.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatelessWidget {
+
+  final TextEditingController name1 = TextEditingController();
+  final TextEditingController name2 = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Love Calculator'),
-        backgroundColor: Colors.pink,
+        elevation: 0.0,
+        backgroundColor: Colors.transparent,
+        centerTitle: true,
       ),
+
       backgroundColor: Colors.red,
       body: Center(
-        child: NameContainer(),
+        child: NameContainer(name1: name1, name2: name2,
+          onTap: () async{
+
+          // Getting the url of the API call
+            var url =
+                'https://rapidapi.p.rapidapi.com/getPercentage?fname=${name1.text.trim()}&sname=${name2.text.trim()}';
+
+          // Getting the host and key headers
+            Map<String, String> headers = {
+              'x-rapidapi-host': 'love-calculator.p.rapidapi.com',
+              'x-rapidapi-key': '40cd906284mshf322f285a1e564dp1aebfdjsn013d55972b98',
+            };
+
+          // await -> Guides the flutter to take time to collect the response
+          var response = await http.get(url, headers: headers);
+          print(name1.text);
+            print(response.body);
+            print('Response is collected.');
+          },
+        ),
       )
     );
   }
--- lib/widgets/name_container.dart
+++ lib/widgets/name_container.dart
@@ -2,6 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:love_calculator/widgets/custom_button.dart';
 
 class NameContainer extends StatelessWidget {
+
+  final TextEditingController name1;
+  final TextEditingController name2;
+  final Function onTap;
+
+  const NameContainer({ this.onTap, this.name1, this.name2});
+
   @override
   Widget build(BuildContext context) {
     return FittedBox(
@@ -34,6 +41,7 @@ class NameContainer extends StatelessWidget {
             ),
             TextField(
               cursorColor: Colors.red,
+              controller: name1,
               decoration: InputDecoration(
                 hintText: 'Enter the first name',
                 hintStyle: TextStyle(fontWeight: FontWeight.w600, color: Colors.red),
@@ -44,12 +52,16 @@ class NameContainer extends StatelessWidget {
             ),
             TextField(
               cursorColor: Colors.red,
+              controller: name2,
               decoration: InputDecoration(
                 hintText: 'Enter the second name',
                 hintStyle: TextStyle(fontWeight: FontWeight.w600, color: Colors.red),
               ),
             ),
-            CustomButton()
+            SizedBox(
+              height: 30,
+            ),
+            CustomButton(onTap: onTap)
           ],
         ),
       ),
